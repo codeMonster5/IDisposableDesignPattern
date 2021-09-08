4fa44d9f270d9f12308ca928cf1edbeaffde1d67
Mani <mani.bhushan@nowfloats.com>
2021-09-08 23:09:44 +0530

    Add project files.

---
+++ IDisposableDesignPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+
+namespace IDisposableDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            using ServiceProxy obj = new ServiceProxy();
+
+            obj.Get("https://alive");
+
+            obj.Post("https://alive","");
+
+            obj.Dispose();
+
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IDisposableDesignPattern/ServiceProxy.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace IDisposableDesignPattern
+{
+    public class ServiceProxy :IDisposable
+    {
+        public readonly HttpClient httpClient;
+
+
+        private bool disposed;
+        public  ServiceProxy()
+        {
+            this.httpClient = new HttpClient();
+        }
+
+        ~ServiceProxy()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                httpClient.Dispose();
+            }
+
+            disposed = true;
+
+        }
+
+        public void Get(string obj)
+        {
+            httpClient.GetAsync(obj);
+        }
+
+        public void Post(string obj,string request)
+        {
+            httpClient.PostAsync(obj, new StringContent(request));
+        }
+    }
+}
